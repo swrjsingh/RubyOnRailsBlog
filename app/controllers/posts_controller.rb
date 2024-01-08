4c95f00884b8637124d3d6a4658a98ffc4656633
Swaraj <swaraj050605@gmail.com>
2024-01-08 12:02:34 +0530

    Add session manager and minor syntax fixes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,16 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
-
+  before_action :authenticate_user!, except: %i[show index]
   # GET /posts or /posts.json
   def index
     @posts = Post.all
   end
-
-  # GET /posts/1 or /posts/1.json
   def show
-      @post.update(views: @post.views +1)
+    views = @post.views + 1
+    @post.views = views
+    @post.save
   end
+  # GET /posts/1 or /posts/1.json
 
   # GET /posts/new
   def new
